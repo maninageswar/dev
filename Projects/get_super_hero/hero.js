5ad6a949e95ff8a519c28e5d3ff7c2e29acc9008
mani-nageswar <mani-nageswar@skillassure.com>
2023-05-31 23:36:43 +0530

    modified the get hero project

--- Projects/get_super_hero/hero.js
+++ Projects/get_super_hero/hero.js
@@ -2,7 +2,12 @@ let container=document.querySelector(".container")
 let HN=document.querySelector(".imagetext")
 let btnPowerStats=document.querySelector(".getpowerstats")
 let ShowPowerStats=document.querySelector(".show_power_stats")
+let pikach=document.querySelector(".pikach")
 let input=document.querySelector("#input")
+let main=document.querySelector(".main")
+let mainunder=document.querySelector(".under")
+let close=document.querySelector(".close")
+let husk=document.querySelector(".husk")
 
 
 // ****************************  THE BELOW CODE IS IMPORTANT TO UNDERSTAND HOW TO GIVE VARIABLES IN URLS    ***************************
@@ -10,18 +15,20 @@ const token='10223569763528853'
 const base_url=`https://www.superheroapi.com/api.php/${token}`
 
 const heroimage=()=>{
+    ShowPowerStats.innerHTML=`<h1></h1>`
     id=Math.floor(Math.random() * 732)
     fetch(`${base_url}/${id}`).then(response=>response.json())
     .then(json=>{
         container.innerHTML=`<img src='${json.image.url}' alt="hero image" width=500; height=500>`
         HN.innerHTML=`${json.name}</h2>`
         btnPowerStats.onclick=()=>{
+            main.classList.add("move");
+            mainunder.classList.add("moveunder");
             console.log(json.powerstats)
             let powerstats=json.powerstats
             keys=Object.keys(powerstats)
             for (let i of keys){
-                // console.log(i+":"+powerstats[`${i}`])
-                ShowPowerStats.innerHTML+=`<p>${i} : ${powerstats[`${i}`]}</p>`
+                ShowPowerStats.innerHTML+=`<h1>${i} : ${powerstats[`${i}`]}</h1>`
             }
         }
     })
@@ -40,6 +47,15 @@ const Searchimage=()=>{
     
 }
 
-// btnPowerStats.onclick=()=>{
-//     console.log("you click")
+
+// function close(){
+    // husk.classList.add("moveopp");
+    // ShowPowerStats.classList.add("moveunderopp");
+//     console.log("you clicked close")
 // }
+close.onclick=()=>{
+    console.log("you clicked")
+    husk.classList.add("moveopp");
+    // pikach.classList.add("moveunderopp");
+    // pikach.remove();
+}
