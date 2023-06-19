462f7952a5ddffff669701456ef1f6072ca2d40c
mani-nageswar <mani-nageswar@skillassure.com>
2023-06-19 13:32:59 +0530

    created python folder and included jupyter notebook in it and made few changes in interface files in typescript folder

---
+++ typescript/interface.js
@@ -0,0 +1,14 @@
+"use strict";
+// interface TakePhoto {
+//     cameraMode:String,
+//     filter:String,
+//     burst:number
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+function test(a, b) {
+    return {
+        a: a,
+        b: b
+    };
+}
+console.log(test(1, "sai"));
---
+++ typescript/interface.ts
@@ -0,0 +1,16 @@
+// interface TakePhoto {
+//     cameraMode:String,
+//     filter:String,
+//     burst:number
+// }
+
+
+function  test(a:number,b:string):object{
+    return{
+        a,b
+    }
+}
+
+console.log(test(1,"sai"));
+
+export {}
